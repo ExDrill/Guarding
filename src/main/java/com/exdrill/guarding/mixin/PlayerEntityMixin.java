3e75094a698c687c9688808a160e8493a5031ab2
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-04-09 00:47:31 -0500

    Update PlayerEntityMixin.java

--- src/main/java/com/exdrill/guarding/mixin/PlayerEntityMixin.java
+++ src/main/java/com/exdrill/guarding/mixin/PlayerEntityMixin.java
@@ -47,14 +47,18 @@ public abstract class PlayerEntityMixin {
     @Inject(method = "takeShieldHit", at = @At("TAIL"))
     private void takeShieldHit(LivingEntity attacker, CallbackInfo ci) {
         System.out.println(parryKnockback);
-        if (shieldParry <= 8 && shieldParry >= 0) {
+        if (shieldParry <= 12 && shieldParry >= 0) {
             attacker.playSound(SoundEvents.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, 0.5F, 1.0F);
             if (ShieldEnchantmentHelper.hasBarbed(player)) {
-                attacker.damage(DamageSource.thorns(attacker), 3.0F);
+                attacker.damage(DamageSource.GENERIC, 3.0F);
                 this.damageShield(2.0F);
             }
-            attacker.takeKnockback( parryKnockback,  -1 * (attacker.getX() - player.getX()), -1 * (attacker.getZ() - player.getZ()));
-            shieldParry = 8;
+            double d = attacker.getX() - player.getX();
+            double e = attacker.getZ() - player.getZ();
+            double f = Math.max(Math.sqrt(d * d + e * e), 0.001D);
+            attacker.addVelocity( d/f * parryKnockback, 0.5D, e/f * parryKnockback);
+            attacker.velocityModified = true;
+            shieldParry = 13;
         }
         if (shieldUseDuration > 100) {
             this.disableShield(false);
