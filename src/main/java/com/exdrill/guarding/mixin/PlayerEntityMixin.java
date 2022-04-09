6380f4e71debf3308d9f6e43950a8b2bfea188e8
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-04-09 12:59:50 -0500

    Update PlayerEntityMixin.java

--- src/main/java/com/exdrill/guarding/mixin/PlayerEntityMixin.java
+++ src/main/java/com/exdrill/guarding/mixin/PlayerEntityMixin.java
@@ -3,7 +3,9 @@ package com.exdrill.guarding.mixin;
 import com.exdrill.guarding.enchantment.ShieldEnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.ItemCooldownManager;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -14,14 +16,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
+
     @Shadow public abstract void disableShield(boolean sprinting);
 
+    public void parryCooldown() {
+        this.getItemCooldownManager().set(Items.SHIELD, 20);
+    }
+
     @Shadow protected abstract void damageShield(float amount);
+
+    @Shadow public abstract ItemCooldownManager getItemCooldownManager();
 
     @Mutable
     public int shieldUseDuration = 0;
     public int shieldParry = 0;
     public double parryKnockback = 0D;
+
     PlayerEntity player = (PlayerEntity) (Object) this;
 
     @Inject(method = "tick", at = @At("TAIL"))
@@ -33,6 +43,7 @@ public abstract class PlayerEntityMixin {
             shieldUseDuration-=2;
             shieldParry = 0;
         }
+
         if (shieldUseDuration <= 0) {
             shieldUseDuration = 0;
         }
@@ -44,21 +55,23 @@ public abstract class PlayerEntityMixin {
         }
     }
 
-    @Inject(method = "takeShieldHit", at = @At("TAIL"))
+    @Inject(method = "takeShieldHit", at = @At("HEAD"))
     private void takeShieldHit(LivingEntity attacker, CallbackInfo ci) {
         System.out.println(parryKnockback);
-        if (shieldParry <= 12 && shieldParry >= 0) {
+        if (shieldParry <= 8 && shieldParry >= 1 && player.isSneaking()) {
             attacker.playSound(SoundEvents.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, 0.5F, 1.0F);
             if (ShieldEnchantmentHelper.hasBarbed(player)) {
-                attacker.damage(DamageSource.GENERIC, 3.0F);
+                attacker.damage(DamageSource.GENERIC, 4.0F);
                 this.damageShield(2.0F);
             }
             double d = attacker.getX() - player.getX();
             double e = attacker.getZ() - player.getZ();
             double f = Math.max(Math.sqrt(d * d + e * e), 0.001D);
-            attacker.addVelocity( d/f * parryKnockback, 0.5D, e/f * parryKnockback);
+            attacker.addVelocity( d/f * parryKnockback, 0.2D, e/f * parryKnockback);
             attacker.velocityModified = true;
+            player.addExhaustion(0.2F);
             shieldParry = 13;
+            this.parryCooldown();
         }
         if (shieldUseDuration > 100) {
             this.disableShield(false);
