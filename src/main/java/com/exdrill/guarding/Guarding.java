329a22f7ee34c33c476e26d17eb5c487dd0b32e5
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-04-11 19:15:20 -0500

    1.1.0

--- src/main/java/com/exdrill/guarding/Guarding.java
+++ src/main/java/com/exdrill/guarding/Guarding.java
@@ -6,12 +6,13 @@ import com.exdrill.guarding.enchantment.ShieldEnchantmentHelper;
 import com.github.crimsondawn45.fabricshieldlib.lib.event.ShieldBlockCallback;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.StatFormatter;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -23,14 +24,24 @@ public class Guarding implements ModInitializer {
     public static final PummelingEnchantment PUMMELING_ENCHANTMENT = new PummelingEnchantment(Enchantment.Rarity.COMMON, false, false);
     public static final BarbedEnchantment BARBED_ENCHANTMENT = new BarbedEnchantment(Enchantment.Rarity.RARE, false, false);
 
+
+
+    public static final Identifier PARRY = new Identifier(NAMESPACE, "parry");
+
     public double parryKnockbackMultiplier = 0D;
 
     @Override
     public void onInitialize() {
         System.out.println("Guarding is loaded");
+        // Enchantments
         Registry.register(Registry.ENCHANTMENT, new Identifier(NAMESPACE, "pummeling"), PUMMELING_ENCHANTMENT);
         Registry.register(Registry.ENCHANTMENT, new Identifier(NAMESPACE, "barbed"), BARBED_ENCHANTMENT);
 
+        // Stats
+        Registry.register(Registry.CUSTOM_STAT, new Identifier(NAMESPACE, "parry"), PARRY);
+        Stats.CUSTOM.getOrCreateStat(PARRY, StatFormatter.DEFAULT);
+
+        // Shield Parrying
         ShieldBlockCallback.EVENT.register((defender, source, amount, hand, shield) -> {
 
             // Living Entity
@@ -38,7 +49,7 @@ public class Guarding implements ModInitializer {
             assert attacker != null;
 
             // Parry
-            if (defender instanceof PlayerEntity player && defender.getItemUseTime() <= 10 && defender.getItemUseTime() >= 1 && defender.isSneaking() && !source.isProjectile()) {
+            if (defender instanceof PlayerEntity player && defender.getItemUseTime() <= 10 && defender.isSneaking() && !source.isProjectile()) {
                 attacker.playSound(SoundEvents.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, 0.5F, 1.0F);
 
                 // Pummeling
@@ -59,18 +70,16 @@ public class Guarding implements ModInitializer {
                 double f = Math.max(Math.sqrt(d * d + e * e), 0.001D);
                 attacker.addVelocity( d/f * parryKnockbackMultiplier, 0.25D, e/f * parryKnockbackMultiplier);
                 attacker.velocityModified = true;
-                ((PlayerEntity) defender).addExhaustion(0.5F);
-                ((PlayerEntity) defender).getItemCooldownManager().set(Items.SHIELD, 20);
-                defender.clearActiveItem();
-                defender.world.sendEntityStatus(player, (byte) 30);
+                ((PlayerEntity) defender).addExhaustion(1.0F);
 
             }
-            if (defender.getItemUseTime() > 100) {
+            if (defender.getItemUseTime() > 200) {
                 assert defender instanceof PlayerEntity;
                 ((PlayerEntity) defender).getItemCooldownManager().set(Items.SHIELD, 100);
                 defender.clearActiveItem();
                 defender.world.sendEntityStatus(defender, (byte) 30);
             }
+
             return ActionResult.success(true);
         });
     }
