a28ef0cdc71e54b6563f947d05569221e48bdd6f
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-10-26 21:48:31 -0500

    Update GuardingEarlyRiser.java

--- src/main/java/com/exdrill/guarding/GuardingEarlyRiser.java
+++ src/main/java/com/exdrill/guarding/GuardingEarlyRiser.java
@@ -1,14 +1,13 @@
 package com.exdrill.guarding;
 
 import com.chocohead.mm.api.ClassTinkerers;
-import net.fabricmc.loader.FabricLoader;
+import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
 
 public class GuardingEarlyRiser implements Runnable {
 
-    @Override
     public void run() {
-        MappingResolver mappingResolver = FabricLoader.INSTANCE.getMappingResolver();
+        MappingResolver mappingResolver = FabricLoader.getInstance().getMappingResolver();
 
         String enchantmentTarget = mappingResolver.mapClassName("intermediary", "net.minecraft.class_1886");
         ClassTinkerers.enumBuilder(enchantmentTarget, new Class[0]).addEnumSubclass("GUARDING_SHIELD", "com.exdrill.guarding.util.GuardingEnchantmentTarget").build();
