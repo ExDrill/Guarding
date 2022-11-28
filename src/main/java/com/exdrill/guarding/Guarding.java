eb7e3ad8a2b1d7b4b5311bd2e44521cdb209687b
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-11-28 15:11:57 -0500

    Start!

---
+++ src/main/java/com/exdrill/guarding/Guarding.java
@@ -0,0 +1,29 @@
+package com.exdrill.guarding;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Guarding.MODID)
+public class Guarding {
+    public static final String MODID = "guarding";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Guarding() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+           LOGGER.debug("Loading up Guarding...");
+        });
+    }
+
+}
